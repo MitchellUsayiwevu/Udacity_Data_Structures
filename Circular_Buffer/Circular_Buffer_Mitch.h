9d1cc0cba333d65729ac3952bb814bb86c7f3653
MitchellUsayiwevu <mitchell.usayiwevu@student.uts.edu.au>
2025-06-04 16:55:04 +1000

    circular buffer implementation

---
+++ Circular_Buffer/Circular_Buffer_Mitch.h
@@ -0,0 +1,20 @@
+//
+// Created by robotics on 6/4/25.
+//
+
+#ifndef UDACITY_DATA_STRUCTURES_CIRCULAR_BUFFER_MITCH_H
+#define UDACITY_DATA_STRUCTURES_CIRCULAR_BUFFER_MITCH_H
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class CircularBuffer{
+private:
+    int count;
+    T* head;
+    T* tail;
+
+};
+
+#endif //UDACITY_DATA_STRUCTURES_CIRCULAR_BUFFER_MITCH_H
